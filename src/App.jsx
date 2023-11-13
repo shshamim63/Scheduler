a6c18627fa2fe08940786238f23f9a9edd3e1c1d
shshamim63 <shshamim63@gmail.com>
2023-11-13 21:19:28 +0600

    Added initial route with create browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Login from "./features/user/Login";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import OrderItem from "./features/order/OrderItem";
+import Registration from "./features/user/Registration";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <Registration /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:id", element: <OrderItem /> },
+    ],
+  },
 ]);
 
 function App() {
