9720521b6ebcc4ae2a1f274a57cbeb0f849acb84
shshamim63 <shshamim63@gmail.com>
2023-11-13 21:20:40 +0600

    Added simple menu component along with individual context

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+
+import { getMenu } from "../../services/apiRestaurants";
+import MenuItem from "./MenuItem";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+};
+
+export const loader = async () => {
+  const menu = await getMenu();
+  return menu;
 };
 
 export default Menu;
