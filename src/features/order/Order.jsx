c6c8232b578229873e1ecd68f9a0b7e7deea22f2
shshamim63 <shshamim63@gmail.com>
2023-11-22 22:53:15 +0600

    Added styling to the order section

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,30 +1,69 @@
-import { useLoaderData } from "react-router-dom";
-import { getOrder } from "../../services/apiRestaurants";
-import OrderItem from "./OrderItem";
+import { useLoaderData } from 'react-router-dom';
+import { getOrder } from '../../services/apiRestaurants';
+
+import OrderItem from './OrderItem';
+
+import { formatDate, calcMinutesLeft } from '../../utils/date';
+import { formatCurrency } from '../../utils/currency';
 
 const Order = () => {
   const order = useLoaderData();
 
-  const { id, status, priority, orderPrice, estimatedDelivery, cart } = order;
+  const {
+    id,
+    status,
+    priority,
+    priorityPrice,
+    estimatedDelivery,
+    orderPrice,
+    cart,
+  } = order;
+
+  const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div>
-      <div>
-        <h2>Order #{id} status</h2>
+    <div className="space-y-8 px-6 py-6">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-xl font-semibold">Order #{id} status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+        <div className="space-x-2">
+          {priority && (
+            <span className="rounded-full bg-red-500 px-3 py-1 text-sm uppercase tracking-wide text-red-50">
+              Priority
+            </span>
+          )}
+          <span className="rounded-full bg-green-500 px-3 py-1 text-sm uppercase tracking-wide text-green-50">
+            {status} order
+          </span>
         </div>
-        <div>
-          <p>Price {orderPrice}</p>
-          <p>{estimatedDelivery}</p>
-        </div>
-        <div>
-          {cart.map((item, index) => (
-            <OrderItem item={item} key={index} />
-          ))}
-        </div>
+      </div>
+      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
+        <p className="font-medium">
+          {deliveryIn >= 0
+            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left`
+            : 'Order should have arrived'}
+        </p>
+        <p className="text-xs text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
+      </div>
+      <ul className="divide-y divide-stone-200 border-b border-t">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.id} />
+        ))}
+      </ul>
+      <div className="space-y-2 bg-stone-200 px-6 py-5">
+        <p className="text-sm font-medium text-stone-600">
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
+        {priority && (
+          <p className="text-sm font-medium text-stone-600">
+            Price priority: {formatCurrency(priorityPrice)}
+          </p>
+        )}
+        <p className="font-bold">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   );
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,15 +1,17 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { formatCurrency } from '../../utils/currency';
 
 const OrderItem = ({ item }) => {
-  const { name, pizzaId, quantity, totalPrice, unitPrice } = item;
+  const { name, quantity, totalPrice } = item;
   return (
-    <div>
-      <p>{name}</p>
-      <p>{pizzaId}</p>
-      <p>{quantity}</p>
-      <p>{totalPrice}</p>
-      <p>{unitPrice}</p>
-    </div>
+    <li className="py-3">
+      <div className="flex items-center justify-between gap-4 text-sm">
+        <p>
+          <span className="font-bold">{quantity}&times;</span> {name}
+        </p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
   );
 };
 
