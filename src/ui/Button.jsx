9d929d5511b1f0b293abe96eefd72dc826b7a51e
shshamim63 <shshamim63@gmail.com>
2023-11-23 20:58:59 +0600

    Added store and created a separate button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -10,6 +10,7 @@ const Button = ({ children, disabled, to, type }) => {
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
     secondary:
       'text-sm inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-200 focus:outlined-none focus: ring focus:ring-yellow-300 focus:ring-offset-2 disabled: cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5 hover:text-stone-800',
+    submit: base + ' px-4 py-2 md:px-5 md:py-2.5 text-sm',
   };
 
   if (to)
@@ -18,7 +19,12 @@ const Button = ({ children, disabled, to, type }) => {
         {children}
       </Link>
     );
-
+  if (type === 'submit')
+    return (
+      <button type="submit" className={styles[type]}>
+        {children}
+      </button>
+    );
   return (
     <button disabled={disabled} className={styles[type]}>
       {children}
