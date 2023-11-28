fb81010957e5445b0f42ffc8dc363e0d8e019eac
shshamim63 <shshamim63@gmail.com>
2023-11-28 19:53:50 +0600

    Started checking looged in user before menu page

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,11 +1,20 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 import { getMenu } from '../../services/apiRestaurants';
 
 import MenuItem from './MenuItem';
+import { useSelector } from 'react-redux';
+import { getUserContext } from '../user/userSlice';
 
 const Menu = () => {
   const menu = useLoaderData();
+  const navigate = useNavigate();
+
+  const { userInfo, status, authError } = useSelector(getUserContext);
+
+  const userLoggedIn = userInfo?.email && status === 'idle' && !authError;
+  console.log('Logged', userInfo, status, authError);
+  if (!userLoggedIn) navigate('/login');
 
   return (
     <ul className="divide-y divide-stone-200 px-2">
